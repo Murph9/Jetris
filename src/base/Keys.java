2321f2ef2ca42dc566fc279fee55394830dab35c
Murph9 <murph9y@gmail.com>
2019-01-17 08:52:06 +1100

    Update key defaults (2)

--- src/base/Keys.java
+++ src/base/Keys.java
@@ -25,10 +25,20 @@ public class Keys implements RawInputListener {
 	
 
 	private void initKeys() {
+		//clockwise
+		keyLayout.put(KeyInput.KEY_UP, "RotateRight");
+		keyLayout.put(KeyInput.KEY_X, "RotateRight"); 
+		
 		keyLayout.put(KeyInput.KEY_SPACE, "HardDrop");
 		
-		keyLayout.put(KeyInput.KEY_DOWN, "Drop");
-		keyLayout.put(KeyInput.KEY_S, "Drop");
+		keyLayout.put(KeyInput.KEY_LSHIFT, "Hold");
+		keyLayout.put(KeyInput.KEY_RSHIFT, "Hold");
+		keyLayout.put(KeyInput.KEY_C, "Hold");
+		
+		//counter-clockwise
+		keyLayout.put(KeyInput.KEY_RCONTROL, "RotateLeft");
+		keyLayout.put(KeyInput.KEY_LCONTROL, "RotateLeft");
+		keyLayout.put(KeyInput.KEY_Z, "RotateLeft");
 	
 		keyLayout.put(KeyInput.KEY_LEFT, "Left");
 		keyLayout.put(KeyInput.KEY_A, "Left");
@@ -36,11 +46,31 @@ public class Keys implements RawInputListener {
 		keyLayout.put(KeyInput.KEY_RIGHT, "Right");
 		keyLayout.put(KeyInput.KEY_D, "Right");
 
-		keyLayout.put(KeyInput.KEY_Z, "RotateLeft");
-		keyLayout.put(KeyInput.KEY_X, "RotateRight");
-
 		keyLayout.put(KeyInput.KEY_P, "Pause");
 		keyLayout.put(KeyInput.KEY_ESCAPE, "Pause");
+		keyLayout.put(KeyInput.KEY_F1, "Pause");
+		
+		keyLayout.put(KeyInput.KEY_DOWN, "Drop");
+		
+		
+		//numpad keys (untested)
+		keyLayout.put(KeyInput.KEY_NUMPAD1, "RotateRight");
+		keyLayout.put(KeyInput.KEY_NUMPAD5, "RotateRight");
+		keyLayout.put(KeyInput.KEY_NUMPAD9, "RotateRight");
+		
+		keyLayout.put(KeyInput.KEY_NUMPAD8, "HardDrop");
+		
+		keyLayout.put(KeyInput.KEY_NUMPAD0, "Hold");
+		
+		keyLayout.put(KeyInput.KEY_NUMPAD3, "RotateLeft");
+		keyLayout.put(KeyInput.KEY_NUMPAD7, "RotateLeft");
+		
+		//no pause on numpad
+		
+		keyLayout.put(KeyInput.KEY_NUMPAD4, "Left");
+		keyLayout.put(KeyInput.KEY_NUMPAD6, "Right");
+		
+		keyLayout.put(KeyInput.KEY_NUMPAD2, "Drop");
 	}
 
 
